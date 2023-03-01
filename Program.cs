3898579021d46480e1780d850f404aaa8bf058a4
Przemysław Onak <onaprz@on.sinch.com>
2023-03-01 17:33:43 +0100

    Add simple textbox to index, and wrap it inside app element

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using blazor_onchange;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.RegisterCustomElement<App>("app-element");
+builder.RootComponents.RegisterCustomElement<blazor_onchange.Pages.Index>("app-element");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
